cab9b4b19d107f266e6b8a857fcaad3b0fc770fd
pemtshewang <pemtshewang500@gmail.com>
2023-10-26 13:18:06 +0600

    push

--- components/grievances-analytics.tsx
+++ components/grievances-analytics.tsx
@@ -11,7 +11,7 @@ import { ChartDisplayAttributeType } from "@/types";
 import { useEffect, useState } from "react";
 
 
-async function getAllChartData({ type }: { type: "all" | "user" }): Promise<ChartDisplayAttributeType[]> {
+async function getAllChartData({ type }: { type: "all" | "user" }) {
   const [result, setResult] = useState([]);
   if (type === "all") {
     const res = await fetch(`https://samchar.vercel.app/api/chart`, {
@@ -34,33 +34,33 @@ async function getAllChartData({ type }: { type: "all" | "user" }): Promise<Char
     const data = await res.json();
     setResult(data);
   }
+  return result;
+}
 
-  export default function GrievianceAnalyticsSection({ type }: { type: "all" | "user" }) {
-    const [chartData, setChartData] = useState<ChartDisplayAttributeType[]>([]);
-    const [loading, setLoading] = useState<boolean>(true);
-    useEffect(() => {
-      getAllChartData({ type }).then((data) => {
-        setChartData(data);
-        setLoading(false);
-      });
-    }, []);
-    return (// components/ScrollArea.js
-      <section className="grid lg:grid-cols-3 sm:flex-col p-3 justify-items-center gap-x-2" >
-        {
-          chartData.map((data, index) => {
-            return (
-              <Card className="p-7 w-full" key={index}>
-                <CardHeader className="p-0 m-0">
-                  <CardTitle >{data.header}</CardTitle>
-                  <CardDescription>{data.description}</CardDescription>
-                </CardHeader>
-                <CardContent className="p-3">
-                  <CustomPieChart data={data.data} />
-                </CardContent>
-              </Card>
-            )
-          })
-        }
-      </section >
-    )
-  }
+export default function GrievianceAnalyticsSection({ type }: { type: "all" | "user" }) {
+  const [chartData, setChartData] = useState<ChartDisplayAttributeType[]>([]);
+  useEffect(() => {
+    getAllChartData({ type }).then((data) => {
+      setChartData(data);
+    });
+  }, []);
+  return (// components/ScrollArea.js
+    <section className="grid lg:grid-cols-3 sm:flex-col p-3 justify-items-center gap-x-2" >
+      {
+        chartData.map((data, index) => {
+          return (
+            <Card className="p-7 w-full" key={index}>
+              <CardHeader className="p-0 m-0">
+                <CardTitle >{data.header}</CardTitle>
+                <CardDescription>{data.description}</CardDescription>
+              </CardHeader>
+              <CardContent className="p-3">
+                <CustomPieChart data={data.data} />
+              </CardContent>
+            </Card>
+          )
+        })
+      }
+    </section >
+  )
+}

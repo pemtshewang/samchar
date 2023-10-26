55e2fd7bef425a94517ef7eed637e2c33dfb86e0
pemtshewang <pemtshewang500@gmail.com>
2023-10-26 19:24:30 +0600

    error fixing

--- app/(admin)/admin/dashboard/grievances/page.tsx
+++ app/(admin)/admin/dashboard/grievances/page.tsx
@@ -4,75 +4,75 @@ import {
   TabsTrigger,
   TabsContent
 } from "@/components/ui/tabs";
-import GrievancesListPage from "@/components/admin/admin-table/npage";
+// import GrievancesListPage from "@/components/admin/admin-table/npage";
 import { db } from "@/lib/db";
 
 export default async function AdminGrievancePage() {
 
-  const totalGrievancesPending = await db.grievance.findMany({
-    where: {
-      status: "Pending",
-    },
-    select: {
-      adminChecked: true,
-      category: true,
-      title: true,
-      description: true,
-      id: true,
-      status: true,
-      datePosted: true,
-    }
-  });
-
-  const totalGrievancesResolved = await db.grievance.findMany({
-    where: {
-      status: "Resolved",
-    },
-    select: {
-      adminChecked: true,
-      category: true,
-      title: true,
-      description: true,
-      id: true,
-      status: true,
-      datePosted: true,
-    }
-  });
-  const totalGrievancesRejected = await db.grievance.findMany({
-    where: {
-      status: "Rejected",
-    },
-    select: {
-      adminChecked: true,
-      category: true,
-      title: true,
-      description: true,
-      id: true,
-      status: true,
-      datePosted: true,
-    }
-  });
-  const totalGrievancesFiltered = await db.grievance.findMany({
-    where: {
-      status: "Filtered",
-    },
-    select: {
-      adminChecked: true,
-      category: true,
-      title: true,
-      description: true,
-      id: true,
-      status: true,
-      datePosted: true,
-    }
-  });
-  const grievances = {
-    grievancesPending: totalGrievancesPending,
-    grievancesResolved: totalGrievancesResolved,
-    grievancesRejected: totalGrievancesRejected,
-    grievancesFiltered: totalGrievancesFiltered,
-  };
-  const { grievancesPending, grievancesResolved, grievancesRejected, grievancesFiltered } = grievances;
+  // const totalGrievancesPending = await db.grievance.findMany({
+  //   where: {
+  //     status: "Pending",
+  //   },
+  //   select: {
+  //     adminChecked: true,
+  //     category: true,
+  //     title: true,
+  //     description: true,
+  //     id: true,
+  //     status: true,
+  //     datePosted: true,
+  //   }
+  // });
+  //
+  // const totalGrievancesResolved = await db.grievance.findMany({
+  //   where: {
+  //     status: "Resolved",
+  //   },
+  //   select: {
+  //     adminChecked: true,
+  //     category: true,
+  //     title: true,
+  //     description: true,
+  //     id: true,
+  //     status: true,
+  //     datePosted: true,
+  //   }
+  // });
+  // const totalGrievancesRejected = await db.grievance.findMany({
+  //   where: {
+  //     status: "Rejected",
+  //   },
+  //   select: {
+  //     adminChecked: true,
+  //     category: true,
+  //     title: true,
+  //     description: true,
+  //     id: true,
+  //     status: true,
+  //     datePosted: true,
+  //   }
+  // });
+  // const totalGrievancesFiltered = await db.grievance.findMany({
+  //   where: {
+  //     status: "Filtered",
+  //   },
+  //   select: {
+  //     adminChecked: true,
+  //     category: true,
+  //     title: true,
+  //     description: true,
+  //     id: true,
+  //     status: true,
+  //     datePosted: true,
+  //   }
+  // });
+  // const grievances = {
+  //   grievancesPending: totalGrievancesPending,
+  //   grievancesResolved: totalGrievancesResolved,
+  //   grievancesRejected: totalGrievancesRejected,
+  //   grievancesFiltered: totalGrievancesFiltered,
+  // };
+  // const { grievancesPending, grievancesResolved, grievancesRejected, grievancesFiltered } = grievances;
   return (
     <Tabs defaultValue="pending" className="space-y-4">
       <TabsList>
@@ -91,22 +91,22 @@ export default async function AdminGrievancePage() {
       </TabsList>
       <TabsContent value="pending" className="space-y-4">
         <div>
-          <GrievancesListPage type="pending" grievances={grievancesPending} />
+          {/* <GrievancesListPage type="pending" grievances={grievancesPending} /> */}
         </div>
       </TabsContent>
       <TabsContent value="resolved" className="space-y-4">
         <div>
-          <GrievancesListPage type="resolved" grievances={grievancesResolved} />
+          {/* <GrievancesListPage type="resolved" grievances={grievancesResolved} /> */}
         </div>
       </TabsContent>
       <TabsContent value="rejected" className="space-y-4">
         <div>
-          <GrievancesListPage type="rejected" grievances={grievancesRejected} />
+          {/* <GrievancesListPage type="rejected" grievances={grievancesRejected} /> */}
         </div>
       </TabsContent>
       <TabsContent value="filtered" className="space-y-4">
         <div>
-          <GrievancesListPage type="filtered" grievances={grievancesFiltered} />
+          {/* <GrievancesListPage type="filtered" grievances={grievancesFiltered} /> */}
         </div>
       </TabsContent>
     </Tabs>

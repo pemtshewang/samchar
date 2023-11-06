5da3f9396a81651c2c753d0f438b89f6d50cfa8b
pemtshewang <pemtshewang500@gmail.com>
2023-11-06 15:23:34 +0600

    changed middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,7 @@
 import { withAuth } from "next-auth/middleware";
 import { getToken } from "next-auth/jwt";
 import { NextResponse, NextRequest } from "next/server";
+
 export default withAuth(
   async function middleware(req: NextRequest) {
     const token = await getToken({ req })
@@ -15,17 +16,31 @@ export default withAuth(
       }
       return null
     }
+
     if (!isAuth) {
       let from = req.nextUrl.pathname;
       if (req.nextUrl.search) {
         from += req.nextUrl.search;
       }
+
       return NextResponse.redirect(
         new URL(`/login?from=${encodeURIComponent(from)}`, req.url)
       );
     }
   },
+  {
+    callbacks: {
+      async authorized() {
+        // This is a work-around for handling redirect on auth pages.
+        // This is a work-around for handling redirect on auth pages.
+        // We return true here so that the middleware function above
+        // is always called.
+        return true
+      },
+    },
+  },
 );
+
 export const config = {
   matcher: ["/dashboard", "/admin"],
 };
